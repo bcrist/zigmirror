9b1c65f5d07ce1bf5bf784cbdbb8151b2bf615ad
B. Crist <git@bcr.ist>
2026-08-04 14:16:51 -0500

    Adjust eviction score alg

--- src/Cache.zig
+++ src/Cache.zig
@@ -348,11 +348,18 @@ pub const Entry = struct {
 
         const time_in_cache: u64 = if (now > first) std.math.cast(u64, now -% first) orelse 0 else 0;
         const time_since_last: u64 = if (now > last) std.math.cast(u64, now -% last) orelse 0 else 0;
-        const dev_penalty: u64 = if (self.artifact != null and self.artifact.?.pre != null) 60_000 else 1000;
         const not_found_penalty: u64 = if (self.bytes == null and self.data == .none) 1_000_000 else 0;
-        const time_factor: u64 = if (self.artifact != null and self.artifact.?.extension.is_minisig()) 2 else 1;
+        var time_factor: u64 = 1;
+        if (self.artifact) |artifact| {
+            if (artifact.extension.is_minisig()) {
+                time_factor *= 4;
+            }
+            if (artifact.pre != null) {
+                time_factor *= 2;
+            }
+        }
 
-        const numer = (time_in_cache + time_since_last) * time_factor + dev_penalty + not_found_penalty;
+        const numer = (time_in_cache + time_since_last) * time_factor + not_found_penalty;
         const denom = if (requests > 1) requests + 10 else 1;
 
         return numer / denom;
